f5fce579d8383de35850f0ad7bb89e5b56646f18
Clashicly Trained <clashicly@gmail.com>
2018-02-26 23:27:57 -0500

    change default data dir from Bitcoin to Clashic on Windows, MacOS and Unix/Linux

--- src/util.cpp
+++ src/util.cpp
@@ -469,13 +469,13 @@ void PrintExceptionContinue(const std::exception *pex, const char *pszThread) {
 
 boost::filesystem::path GetDefaultDataDir() {
     namespace fs = boost::filesystem;
-// Windows < Vista: C:\Documents and Settings\Username\Application Data\Bitcoin
-// Windows >= Vista: C:\Users\Username\AppData\Roaming\Bitcoin
-// Mac: ~/Library/Application Support/Bitcoin
-// Unix: ~/.bitcoin
+// Windows < Vista: C:\Documents and Settings\Username\Application Data\Clashic
+// Windows >= Vista: C:\Users\Username\AppData\Roaming\Clashic
+// Mac: ~/Library/Application Support/Clashic
+// Unix: ~/.clashic
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Bitcoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "Clashic";
 #else
     fs::path pathRet;
     char *pszHome = getenv("HOME");
@@ -485,10 +485,10 @@ boost::filesystem::path GetDefaultDataDir() {
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/Bitcoin";
+    return pathRet / "Library/Application Support/Clashic";
 #else
     // Unix
-    return pathRet / ".bitcoin";
+    return pathRet / ".clashic";
 #endif
 #endif
 }
