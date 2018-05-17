b8f3895063769ead57226c0d6ab10e21d459c7f9
Clashicly Trained <clashicly@gmail.com>
2018-05-16 21:20:45 -0400

    Core HF activation code

--- src/validation.h
+++ src/validation.h
@@ -374,6 +374,9 @@ void PruneBlockFilesManual(int nPruneUpToHeight);
 /** Check is UAHF has activated. */
 bool IsUAHFenabled(const Config &config, const CBlockIndex *pindexPrev);
 
+/** Check is Core HF has activated. */
+bool IsCoreHFEnabled(const Config &config, const CBlockIndex *pindexPrev);
+
 /** (try to) add transaction to memory pool
  * plTxnReplaced will be appended to with all transactions replaced from mempool
  * **/
