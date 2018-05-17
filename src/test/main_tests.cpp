8cb08a8c4c8e687b240eb682dcdbb10208309796
Clashicly Trained <clashicly@gmail.com>
2018-05-16 22:31:50 -0400

    backport: Convert validation.cpp/.h to use Amount class

--- src/test/main_tests.cpp
+++ src/test/main_tests.cpp
@@ -15,13 +15,13 @@ BOOST_FIXTURE_TEST_SUITE(main_tests, TestingSetup)
 
 static void TestBlockSubsidyHalvings(const Consensus::Params &consensusParams) {
     int maxHalvings = 64;
-    CAmount nInitialSubsidy = 50 * COIN.GetSatoshis();
+    Amount nInitialSubsidy = 50 * COIN;
 
-    CAmount nPreviousSubsidy = nInitialSubsidy * 2; // for height == 0
-    BOOST_CHECK_EQUAL(nPreviousSubsidy, nInitialSubsidy * 2);
+    Amount nPreviousSubsidy = 2 * nInitialSubsidy; // for height == 0
+    BOOST_CHECK_EQUAL(nPreviousSubsidy, 2 * nInitialSubsidy);
     for (int nHalvings = 0; nHalvings < maxHalvings; nHalvings++) {
         int nHeight = nHalvings * consensusParams.nSubsidyHalvingInterval;
-        CAmount nSubsidy = GetBlockSubsidy(nHeight, consensusParams);
+        Amount nSubsidy = GetBlockSubsidy(nHeight, consensusParams);
         BOOST_CHECK(nSubsidy <= nInitialSubsidy);
         BOOST_CHECK_EQUAL(nSubsidy, nPreviousSubsidy / 2);
         nPreviousSubsidy = nSubsidy;
