ee2e94e3599fbd6abcca9740fdfa2762fa642d6a
corelyone <corelyone@gmail.com>
2018-05-17 09:39:33 -0500

    backport Implement simple moving average over work difficulty adjustement algorithm.

--- src/test/main_tests.cpp
+++ src/test/main_tests.cpp
@@ -48,11 +48,11 @@ BOOST_AUTO_TEST_CASE(block_subsidy_test) {
 BOOST_AUTO_TEST_CASE(subsidy_limit_test) {
     const Consensus::Params &consensusParams =
         Params(CBaseChainParams::MAIN).GetConsensus();
-    CAmount nSum = 0;
+    Amount nSum = 0;
     for (int nHeight = 0; nHeight < 14000000; nHeight += 1000) {
-        CAmount nSubsidy = GetBlockSubsidy(nHeight, consensusParams);
+        Amount nSubsidy = GetBlockSubsidy(nHeight, consensusParams);
         BOOST_CHECK(nSubsidy <= 50 * COIN);
-        nSum += nSubsidy * 1000;
+        nSum += 1000 * nSubsidy;
         BOOST_CHECK(MoneyRange(nSum));
     }
     BOOST_CHECK_EQUAL(nSum, 2099999997690000ULL);
