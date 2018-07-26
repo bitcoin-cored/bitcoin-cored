6dbc856c215e823c9cc6bc2ab5a74de9507dc3ff
corelyone <corelyone@gmail.com>
2018-07-26 15:51:51 -0500

    update seeder user agent version and remove cash testnet seeders

--- src/seeder/bitcoin.cpp
+++ src/seeder/bitcoin.cpp
@@ -92,7 +92,7 @@ class CNode {
         CAddress me(CService("0.0.0.0"));
         BeginMessage("version");
         int nBestHeight = GetRequireHeight();
-        std::string ver = "/bitcoin-core-sq-seeder:0.16.1/";
+        std::string ver = "/bitcoin-core-sq-seeder:0.16.2/";
         vSend << PROTOCOL_VERSION << nLocalServices << nTime << you << me
               << nLocalNonce << ver << nBestHeight;
         EndMessage();
